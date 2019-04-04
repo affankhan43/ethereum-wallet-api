00eacb51474b923c5fbc401890c0c63eba0913c9
unknown <affankhan627@gmail.com>
2019-04-04 09:53:27 +0500

    eth

--- ethereum-wallet.go
+++ ethereum-wallet.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"os"
 	"fmt"
 	"log"
-	"context"
+	"bytes"
+	"reflect"
+	//"context"
 	"crypto/ecdsa"
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 type Required struct {
@@ -48,7 +54,73 @@ func CreateAddress(c *gin.Context) {
 					"success":false,
 					"message":"Address Generation Failed",
 				})
+			} else {
+				privateKeyBytes := crypto.FromECDSA(privateKey)
+				publicKey := privateKey.Public()
+				publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+				if !ok {
+					log.Fatal("error casting public key to ECDSA")
+				}
+				publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+				fmt.Println(hexutil.Encode(publicKeyBytes)[4:])
+				pvKey:=hexutil.Encode(privateKeyBytes)
+				pvEn := SplitSubN(pvKey,35)
+				godotenv.Load()
+				address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+				qrr, err := db.Query("INSERT INTO keystore (`label`,`address`,`key_data`,`key_next`) values ('test',?,hex(aes_encrypt(?,?)),hex(aes_encrypt(?,?)));",address,pvEn[0],os.Getenv("Secret_Key1"),pvEn[1],os.Getenv("Secret_Key2"))
+				if err != nil {
+					fmt.Println(err)
+					fmt.Println(qrr)
+					c.JSON(200,gin.H{
+						"success":false,
+						"message":"DB Insert Failed",
+					})
+				} else {
+					c.JSON(200,gin.H{
+						"success":true,
+						"address":address,
+						"pv":pvKey,
+					})
+				}
 			}
 		}
 	}
+}
+
+
+func in_array(val interface{}, array interface{}) (exists bool) {
+	exists = false
+	//index = -1
+
+	switch reflect.TypeOf(array).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(array)
+
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(val, s.Index(i).Interface()) == true {
+				// index = i
+				exists = true
+				return
+			}
+		}
+	}
+	return
+}
+
+func SplitSubN(s string, n int) []string {
+	sub := ""
+	subs := []string{}
+
+	runes := bytes.Runes([]byte(s))
+	l := len(runes)
+	for i, r := range runes {
+		sub = sub + string(r)
+		if (i + 1) % n == 0 {
+			subs = append(subs, sub)
+			sub = ""
+		} else if (i + 1) == l {
+			subs = append(subs, sub)
+		}
+	}
+	return subs
 }
